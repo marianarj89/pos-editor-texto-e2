1e45fbf300d9695cbee2111c3924963b2e23942b
Mariana Ribas <mariana.rj89@gmail.com>
2023-10-08 00:56:41 -0300

    v3 - MONGOOSE

--- server/src/Index.ts
+++ server/src/Index.ts
@@ -3,6 +3,17 @@ import http from "http";
 import Delta from "quill-delta";
 import DocumentModel from "./Documento";
 
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1/pos-fs-editor-docs")
+.then(() => {
+    console.log('Banco conectado');
+})
+.catch(() => {
+    console.log('erro ao conectar no banco');
+});
+
+
 async function findOrCreateDocument(id: string) {
     // if (id == null) return
 
@@ -25,6 +36,11 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket: Socket) => {
+    socket.on("get-document", async (id: string) => {
+        const document = await findOrCreateDocument(id);
+        socket.emit("document", document.data);
+    })
+
     socket.on("send-changes", (delta: Delta) => {
         socket.broadcast.emit("receive-changes", delta);
     })
